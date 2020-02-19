6cf79b79ddd21a28a97246ee4e1d6d18b0bef3a1
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-02-19 18:26:35 +0200

    Adding new books to the Array

--- springboot-in-10-steps/src/main/java/com/empact/spring/basis/springbootin10steps/BookController.java
+++ springboot-in-10-steps/src/main/java/com/empact/spring/basis/springbootin10steps/BookController.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 	@GetMapping("/books")
 	public List<Book> getAllBooks(){
-		return Arrays.asList(new Book(1l, "Spring Boot 5.0", "John Brooks"));
+		return Arrays.asList(new Book(1l, "Spring Boot 5.0", "John Brooks"),
+				new Book(2l, "Spring MVC", "John Holt"),
+				new Book(3l, "Spring Batch", "Toyinbo Holt"),
+				new Book(4l, "SAP HANA", "Faith Umukoro"),
+				new Book(5l, "RestFul API", "Emmanuel Adedeji"));
 	}
 
 }
